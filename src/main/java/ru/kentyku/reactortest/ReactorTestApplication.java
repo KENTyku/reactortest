5abb1b67532e8f6e47c2ff288e2cdeff796d3ebf
Yury Tveritin <kentyku@bk.ru>
2020-04-04 23:54:29 +0400

    Add simple FluxTest

---
+++ src/main/java/ru/kentyku/reactortest/ReactorTestApplication.java
@@ -0,0 +1,13 @@
+package ru.kentyku.reactortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kentyku/reactortest/ReactortestApplicationTests.java
@@ -0,0 +1,134 @@
+package ru.kentyku.reactortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+
+@SpringBootTest
+class ReactortestApplicationTests {
+
+	@Test
+	void createFluxTest() {
+		Flux<Integer> ints = Flux.range(1, 3);
+		ints.subscribe(i -> System.out.println(i));
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	@Test
+    void createFluxWithErrorTest(){
+        Flux<Integer> ints = Flux.range(1, 4)
+                .map(i -> {
+                    if (i <= 3) {
+                        return i;
+                    }
+                    throw new RuntimeException("Got to 4");
+                });
+        ints.subscribe(
+                i -> System.out.println(i), error -> System.err.println("Error: " + error)
+        );
+    }
+}

bcb2892fbd0e776af5489220f87f40457943c95f
Yury Tveritin <kentyku@bk.ru>
2020-05-25 18:12:58 +0400

    Fix codeStyle

--- src/test/java/ru/kentyku/reactortest/ReactortestApplicationTests.java
+++ src/test/java/ru/kentyku/reactortest/ReactortestApplicationTests.java
@@ -1,7 +1,6 @@
 package ru.kentyku.reactortest;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.aop.target.EmptyTargetSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -12,14 +11,14 @@ import java.util.Objects;
 @SpringBootTest
 class ReactortestApplicationTests {
 
-	@Test
-	void createFluxTest() {
-		Flux<Integer> ints = Flux.range(1, 3);
-		ints.subscribe(i -> System.out.println(i));
-	}
+    @Test
+    void createFluxTest() {
+        Flux<Integer> ints = Flux.range(1, 3);
+        ints.subscribe(i -> System.out.println(i));
+    }
 
-	@Test
-    void createFluxWithErrorTest(){
+    @Test
+    void createFluxWithErrorTest() {
         Flux<Integer> ints = Flux.range(1, 4)
                 .map(i -> {
                     if (i <= 3) {
@@ -33,20 +32,20 @@ class ReactortestApplicationTests {
     }
 
     @Test
-    void checkMap(){
-        Flux<Cat> cats=Flux.just(new Cat("Barsik",1),
-                new Cat("Vasia",3));
-        Flux<String> names=cats.map(cat->cat.getName());
+    void checkMap() {
+        Flux<Cat> cats = Flux.just(new Cat("Barsik", 1),
+                new Cat("Vasia", 3));
+        Flux<String> names = cats.map(cat -> cat.getName());
 
         StepVerifier.create(names)
                 .expectNext("Barsik", "Vasia")
                 .expectComplete().verify();
-        names.subscribe(i->System.out.println(i));
+        names.subscribe(i -> System.out.println(i));
     }
 
     @Test
-    void checkDefer(){
-        Mono<Cat> cat=Mono.just(new Cat("Barsik", 1));
+    void checkDefer() {
+        Mono<Cat> cat = Mono.just(new Cat("Barsik", 1));
         Mono<String> name = cat.map(c -> c.getName());
 
         Mono<String> defMono = Mono.defer(() -> name.map(n -> n + "Test"));
@@ -54,20 +53,19 @@ class ReactortestApplicationTests {
         StepVerifier.create(defMono)
                 .expectNext("BarsikTest")
                 .expectComplete().verify();
-        defMono.subscribe(i->System.out.println(i));
+        defMono.subscribe(i -> System.out.println(i));
     }
 
     @Test
-
-    void checkFromRunnableAndThen(){
-        Mono<Cat> cat=Mono.just(new Cat("Barsik", 1));
+    void checkFromRunnableAndThen() {
+        Mono<Cat> cat = Mono.just(new Cat("Barsik", 1));
         Mono<String> name = cat.map(c -> c.getName());
-        Mono<Void> emptyMono=Mono
-                .fromRunnable(()->System.out.println("Выполним что-то без " +
-                "входных параметров и результата"));//fromRunnable- создает пустой моно после выполнения кода
+        Mono<Void> emptyMono = Mono
+                .fromRunnable(() -> System.out.println("Выполним что-то без " +
+                        "входных параметров и результата"));//fromRunnable- создает пустой моно после выполнения кода
         emptyMono
                 .then(name)//then - испускает новый элемент,завершив текущий
-                .subscribe(n->System.out.println(n));
+                .subscribe(n -> System.out.println(n));
 
         StepVerifier.create(emptyMono)
                 .expectComplete().verify();
@@ -79,9 +77,8 @@ class ReactortestApplicationTests {
     }
 
     @Test
-
-    void handleError(){
-        Mono<Cat> cat=Mono.just(new Cat("Barsik", 1));
+    void handleError() {
+        Mono<Cat> cat = Mono.just(new Cat("Barsik", 1));
         Mono<String> name = cat.map(c -> c.getName());
 
         Mono<String> resultMono = name
@@ -95,7 +92,7 @@ class ReactortestApplicationTests {
                 .onErrorReturn("BarsikAfterError");
 
 
-        resultMono.subscribe(n-> System.out.println(n));
+        resultMono.subscribe(n -> System.out.println(n));
 
         StepVerifier.create(name)
                 .expectNext("Barsik")
@@ -109,9 +106,8 @@ class ReactortestApplicationTests {
     }
 
     @Test
-
-    void throwError(){
-        Mono<Cat> cat=Mono.just(new Cat("Barsik", 1));
+    void throwError() {
+        Mono<Cat> cat = Mono.just(new Cat("Barsik", 1));
         Mono<String> name = cat.map(c -> c.getName());
 
         Mono<String> resultMono = name
